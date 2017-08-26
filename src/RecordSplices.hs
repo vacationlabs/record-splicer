207fdc24506a7e8d5f65767c5ae2f23b7600022d
pranaysashank <pranaysashank@gmail.com>
2017-08-26 17:10:49 +0530

    Code refactoring

--- src/RecordSplices.hs
+++ src/RecordSplices.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RecordWildCards #-}
 module RecordSplices where
 
@@ -28,13 +27,10 @@ createRecordSplice args = do
       let tName = mkName $ targetName args
           dName = mkName $ targetName args ++ "Delta"
           -- List of type names used in the type synonym, with the data type as the first parameter
-          paramTypeNames = traceShow (({- map (mkName . nameBase) -}(requiredFields args))) getTypes ty
+          paramTypeNames = getTypes ty
 
       -- The data type info of the type synonym
-      dataInfo <- reify $ paramTypeNames !! 0
-
-      -- The list of type variables corresponding to the required fields
-      fieldTypeVars <- getFieldsTypeVar $ (requiredFields args)
+      dataInfo <- reify $ head paramTypeNames
 
       -- The type variables in the data type
       let (ctx, paramVars, kind, fields, classes) = case dataInfo of
@@ -42,11 +38,8 @@ createRecordSplice args = do
           -- An associative list of the type variables in data types and the exact types
           -- in the type synonym
           assocListNamesTyVars =  getTypeVars paramVars (drop 1 paramTypeNames)
-      -- TODO : Find a name for each Type Variable in the fieldtypevars list, a
-      --        corresponding name from the assoc list.
 
           (targetFields, deltaFields) = generateTargetFields' args assocListNamesTyVars fields
-          fieldTypes = traceShow (targetFields) map (getFieldType assocListNamesTyVars) fieldTypeVars
 
       return
         [
@@ -56,13 +49,8 @@ createRecordSplice args = do
 
 sourceToTargetName :: SpliceArgs -> Name -> Name
 sourceToTargetName SpliceArgs{..} name =
-  mkName $ targetPrefix ++ (DL.drop (DL.length sourcePrefix) (nameBase name))
+  mkName $ targetPrefix ++ DL.drop (DL.length sourcePrefix) (nameBase name)
 
-generateTargetFields :: SpliceArgs -> [Name] -> [Type] -> [VarBangType]
-generateTargetFields _ [] _ = []
-generateTargetFields args ((n:ns)) (t:ts) = ((sourceToTargetName args n), bang, t): (generateTargetFields args ns ts)
-                     where
-                       bang = Bang NoSourceUnpackedness NoSourceStrictness
 
 getTypes :: Type -> [Name]
 -- getTypes (AppT (ConT cons) (ConT var)) = [var]
@@ -70,34 +58,19 @@ getTypes (AppT x y) = getTypes x ++ getTypes y
 getTypes (ConT name) = [name]
 
 getFieldType :: [(TyVarBndr, Name)] -> Type -> Type
-getFieldType (((KindedTV n1 kind), name) : xs) t@(VarT n2) | n1 == n2 = ConT name
+getFieldType ((KindedTV n1 kind, name) : xs) t@(VarT n2) | n1 == n2 = ConT name
                                                            | otherwise = getFieldType xs t
 getFieldType _ t@(ConT n2) = t
 
 getTypeVars :: [TyVarBndr] -> [Name] -> [(TyVarBndr, Name)]
 getTypeVars = zip
 
-getFieldTypeVar :: Name -> Q Type
-getFieldTypeVar name = do
-  info <- reify name
-  case info of
-    VarI _ (ForallT _ [] (AppT _ v)) Nothing -> return $ v
-
-getFieldsTypeVar :: [Name] -> Q [Type]
-getFieldsTypeVar names = (mapM getFieldTypeVar names)
-
 generateTargetFields' :: SpliceArgs -> [(TyVarBndr, Name)] -> [VarBangType] -> ([VarBangType], [VarBangType])
-generateTargetFields' args assocList fields = (targetNewFields, targetDeltaFields)
+generateTargetFields' args assocList fields = gen' reqFields fields
                       where
-                        gen _ [] = []
-                        gen reqFields ((v, b, t) : vbts) = if v `elem` reqFields
-                                                                then ((sourceToTargetName args v,b,getFieldType assocList t) : gen reqFields vbts)
-                                                                else gen reqFields vbts
-
-                        genDelta _ [] = []
-                        genDelta reqFields ((v,b,t) : vbts) = if v `elem` reqFields
-                                                                              then genDelta reqFields vbts
-                                                                              else (sourceToTargetName args v, b, getFieldType assocList t) : genDelta reqFields vbts
+                        gen' :: [Name] -> [VarBangType] -> ([VarBangType],[VarBangType])
+                        gen' _ [] = ([],[])
+                        gen' reqFields ((v,b,t):vbts) = if v `elem` reqFields
+                                                           then ((sourceToTargetName args v, b, getFieldType assocList t) : fst (gen' reqFields vbts), snd $ gen' reqFields vbts)
+                                                           else (fst $ gen' reqFields vbts, (sourceToTargetName args v, b, getFieldType assocList t) : snd (gen' reqFields vbts))
                         reqFields = requiredFields args
-                        targetNewFields = gen reqFields fields
-                        targetDeltaFields = genDelta reqFields fields
